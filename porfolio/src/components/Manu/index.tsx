d2b98ff87fb719e3d099e2915ff92cd9a0586862
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2022-12-30 08:50:15 -0300

    feat: create components menu

---
+++ porfolio/src/components/Manu/index.tsx
@@ -0,0 +1,15 @@
+import styles from './Menu.module.scss';
+
+const Menu = () => {
+    return (
+        <>
+            <section className={styles.menu__container}>
+                <div className="">ICON REDES</div>
+                <div className="">LOGO</div>
+                <div className="">FONE CONTATO API</div>
+            </section>
+        </>
+    )
+}
+
+export default Menu
---
+++ porfolio/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Menu from "../../components/Manu"
+
+const Home = () => {
+    return (
+        <>
+            <section>
+                <Menu />
+            </section>
+        </>
+    )
+}
+
+export default Home
