1c9b0459cc64a9d0743cf1f922a3fcd6ed2118b5
MarceloALMoreira <90574339+MarceloALMoreira@users.noreply.github.com>
2022-12-30 21:20:35 -0300

    feat props botão

---
+++ porfolio/src/components/Botao/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Botao.module.scss'
+
+
+// vou criar uma props para botão
+class Botao extends React.Component<any, any>{
+    render() {
+        return (
+            <button className={styles.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+export default Botao
--- porfolio/src/pages/Home/index.tsx
+++ porfolio/src/pages/Home/index.tsx
@@ -1,12 +1,18 @@
+import Botao from "../../components/Botao"
 import Menu from "../../components/Manu"
 import Navs from "../../components/Nav_menu"
 
+   
 const Home = () => {
     return (
         <>
             <section>
                 <Menu />
                 <Navs />
+                {/* Component Botão por children, eu posso usar por atributo tambem! */}
+                <Botao onClick={ teste}>
+                    Dominó
+                </Botao>
             </section>
         </>
     )
